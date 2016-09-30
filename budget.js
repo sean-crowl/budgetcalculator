2d8cf1063edf40f7f4dcd6bf7c6464fa86f9c9d1
Sean Crowl <sean.crowl@interapthq.com>
2016-09-30 15:16:34 -0400

    Sum finally working

--- budget.js
+++ budget.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 });
 
 function createRow() {
-$("#budgetbody").append('<tr> <td style="border: 2px dotted black; padding-top: 10px; padding-bottom: 10px; padding-right: 30px; padding-left: 30px;"><input type="text" class="summary" placeholder="Enter bill summary""></input></td> <td style="border: 2px dotted black; padding-top: 10px; padding-bottom: 10px; padding-right: 30px; padding-left: 30px;"><input onkeypress="return isNumberKey(event)" type="text" class="amount" placeholder="Enter bill amount"></input></td> </tr>');
+$("#budgetbody").append('<tr> <td style="border: 2px dotted black; padding-top: 10px; padding-bottom: 10px; padding-right: 30px; padding-left: 30px;"><input type="text" class="summary" placeholder="Enter bill summary""></input></td> <td style="border: 2px dotted black; padding-top: 10px; padding-bottom: 10px; padding-right: 30px; padding-left: 30px; "><input onkeypress="return isNumberKey(event) " type="text" name="amount" class="amount" placeholder="Enter bill amount "></input></td> </tr>');
 
 }
 
@@ -18,8 +18,40 @@ function isNumberKey(evt)
 
           return true;
        }
-
        
+function addMath() {
+    $(".amount").each(function () {
+            calculateSum();
+            $("#total").show();
+           });
+
+}
+
+function calculateSum() {
+    var sum = 0;
+    //iterate through each textboxes and add the values
+    $(".amount").each(function () {
+
+        //add only if the value is number
+        if (!isNaN(this.value) && this.value.length> 0) {
+            sum += parseFloat(this.value);
+        }
+
+    });
+    //.toFixed() method will roundoff the final sum to 2 decimal places
+    $("#total").val(sum.toFixed(2));
+}
+
+
+       /* function addMath(){
+    var arr = document.getElementsByName('amount');
+    var tot=0;
+    for(var i=0;i<(arr.length);i++){
+        if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = tot; 
+} */
 
 $(function () {
     $("#add").on("click")
